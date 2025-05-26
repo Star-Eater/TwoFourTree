452cc2ddfe3346dd3f6ac9cc8a703bc69902b8be
Elijah Johnson <elijohnson626@gmail.com>
2025-05-26 01:53:23 -0400

    Start

--- src/TwoFourTree.java
+++ src/TwoFourTree.java
@@ -1,6 +1,6 @@
 public class TwoFourTree {
     private class TwoFourTreeItem {
-        int values = 1;
+        int values = 1;                             // Values is the number of values within the node Either 1,2, or 3.
         int value1 = 0;                             // always exists.
         int value2 = 0;                             // exists iff the node is a 3-node or 4-node.
         int value3 = 0;                             // exists iff the node is a 4-node.
@@ -13,32 +13,43 @@ public class TwoFourTree {
         TwoFourTreeItem centerLeftChild = null;     // center-left and center-right children exist iff the node is a non-leaf 4-node.
         TwoFourTreeItem centerRightChild = null;
 
-        public boolean isTwoNode() {
+        // # of values + 1 = Name of the number node
+
+        public boolean isTwoNode() { // Two node: 1 value within the node
+            if(values == 1) return true;  
             return false;
         }
 
-        public boolean isThreeNode() {
+        public boolean isThreeNode() { // Three node: 2 values within the node
+            if (values == 2) return true;
             return false;
         }
 
-        public boolean isFourNode() {
+        public boolean isFourNode() { // Four node: 3 values within the node (Full)
+            if(values == 3) return true;
             return false;
         }
 
-        public boolean isRoot() {
+        public boolean isRoot() { // Checks if the node is the root node
+            if(parent == null) return true;
             return false;
         }
 
-        public TwoFourTreeItem(int value1) {
-
+        public TwoFourTreeItem(int value1) { // Constructor for a two node. When 1 value is created on the spot.
+            this.value1 = value1;
         }
 
-        public TwoFourTreeItem(int value1, int value2) {
-            
+        public TwoFourTreeItem(int value1, int value2) { // Constructor for a three node. When 2 value is created on the spot.
+            this.value1 = value1;
+            this.value2 = value2;
+            values = 2;
         }
 
-        public TwoFourTreeItem(int value1, int value2, int value3) {
-            
+        public TwoFourTreeItem(int value1, int value2, int value3) { // Constructor for a four node. When 1 value is created on the spot.
+            this.value1 = value1;
+            this.value2 = value2;
+            this.value3 = value3;
+            values = 3;
         }
 
         private void printIndents(int indent) {
@@ -67,18 +78,22 @@ public class TwoFourTree {
 
     TwoFourTreeItem root = null;
 
+    // Adding a value to the tree
     public boolean addValue(int value) {
         return false;
     }
 
+    // Searching for a value in the tree
     public boolean hasValue(int value) {
         return false;
     }
 
+    // Deleting a value from the tree
     public boolean deleteValue(int value) {
         return false;
     }
 
+    // Printing the tree in order
     public void printInOrder() {
         if(root != null) root.printInOrder(0);
     }
